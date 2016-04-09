d6e3152d97600e30ee0b737e2d012848b3960cd3
cys <chaoys155@gmail.com>
2016-04-09 23:05:52 +0800

    fix update

--- kernel/bootloader/update-boot.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash -x
-
-IMG=../boot.img
-BIN=./boot.bin
-
-dd if=$BIN of=$IMG conv=notrunc
-
--- kernel/kernel/update-kernel.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash -x
-
-IMG=../boot.img
-
-dd if=kernel.elf of=$IMG seek=1 conv=notrunc
